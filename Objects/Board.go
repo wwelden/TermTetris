6a181b2a5baf0eadee9d25d90a69936ae8e310a4
wwelden <wwelden@trinity.edu>
2024-12-26 21:29:09 -0500

    falling piece, but doesn't clear path

--- Objects/Board.go
+++ Objects/Board.go
@@ -5,3 +5,7 @@ type Board struct {
 	Height int
 	Brd    [][]byte
 }
+
+func (b *Board) Set(pos Position, cell byte) {
+	b.Brd[pos.Y][pos.X] = cell
+}
--- Objects/Game.go
+++ Objects/Game.go
@@ -11,6 +11,7 @@ type Game struct {
 	isRunning  bool
 	GameBoard  *Board
 	DrawBuffer *bytes.Buffer
+	Pieces     []*Piece
 }
 
 func (g *Game) Render() {
@@ -25,16 +26,23 @@ func (g *Game) FillBoard(width, height int) {
 	for i := range g.GameBoard.Brd {
 		g.GameBoard.Brd[i] = make([]byte, width)
 		for j := range g.GameBoard.Brd[i] {
-			g.GameBoard.Brd[i][j] = EmptyCell
+			if i == height-1 || j == 0 || j == width-1 {
+				g.GameBoard.Brd[i][j] = WallCell
+			} else {
+				g.GameBoard.Brd[i][j] = EmptyCell
+			}
 		}
+		// g.GameBoard.Brd[i][width-1] = WallCell
 	}
 }
 
 const (
 	EmptyCell       = 0
 	BlockCell       = 1
-	EmptyCellSymbol = " "
+	WallCell        = 2
+	EmptyCellSymbol = "  "
 	BlockCellSymbol = "🔳"
+	WallCellSymbol  = "⬜️"
 )
 
 func (g *Game) RenderBoard() {
@@ -42,8 +50,10 @@ func (g *Game) RenderBoard() {
 		for _, cell := range row {
 			if cell == EmptyCell {
 				g.DrawBuffer.WriteString(EmptyCellSymbol)
-			} else {
+			} else if cell == BlockCell {
 				g.DrawBuffer.WriteString(BlockCellSymbol)
+			} else if cell == WallCell {
+				g.DrawBuffer.WriteString(WallCellSymbol)
 			}
 		}
 		g.DrawBuffer.WriteString("\n")
@@ -54,15 +64,92 @@ func (g *Game) Start() {
 	g.isRunning = true
 	g.FillBoard(g.GameBoard.Width, g.GameBoard.Height)
 	g.loop()
+
 }
 func (g *Game) Update() {
 	//nothing for now
+
+}
+
+func (g *Game) SpawnPiece() {
+	g.Pieces = append(g.Pieces, &Piece{Position: Position{X: 10, Y: 0}, Shape: Shape1})
+}
+
+func (g *Game) RenderPiece(pos Position) {
+	for _, piece := range g.Pieces {
+		for i, row := range piece.Shape {
+			for j, cell := range row {
+				if cell == "🔳" {
+					g.GameBoard.Set(Position{
+						X: piece.Position.X + j,
+						Y: piece.Position.Y + i,
+					}, BlockCell)
+				}
+			}
+		}
+	}
+}
+func (g *Game) ClearPiece(pos Position) {
+	g.GameBoard.Set(pos, EmptyCell)
+}
+
+func (g *Game) UpdatePiece() {
+	for _, piece := range g.Pieces {
+		for i, row := range piece.Shape {
+			for j, cell := range row {
+				if cell == "🔳" {
+					g.GameBoard.Set(Position{
+						X: piece.Position.X + j,
+						Y: piece.Position.Y + i,
+					}, EmptyCell)
+				}
+			}
+		}
+
+		canFall := true
+		for i, row := range piece.Shape {
+			for j, cell := range row {
+				if cell == "🔳" {
+					nextPos := Position{
+						X: piece.Position.X + j,
+						Y: piece.Position.Y + i + 1,
+					}
+					if nextPos.Y >= g.GameBoard.Height || g.GameBoard.Brd[nextPos.Y][nextPos.X] == BlockCell || g.GameBoard.Brd[nextPos.Y][nextPos.X] == WallCell {
+						canFall = false
+						break
+					}
+				}
+			}
+			if !canFall {
+				break
+			}
+		}
+
+		if canFall {
+			piece.Position.Fall()
+		}
+
+		// Render piece at new position
+		for i, row := range piece.Shape {
+			for j, cell := range row {
+				if cell == "🔳" {
+					g.GameBoard.Set(Position{
+						X: piece.Position.X + j,
+						Y: piece.Position.Y + i,
+					}, BlockCell)
+				}
+			}
+		}
+	}
 }
 
 func (g *Game) loop() {
 	for g.isRunning {
-		g.Update()
 		g.Render()
+		g.SpawnPiece()
+		g.RenderPiece(Position{X: 10, Y: 0})
+		g.UpdatePiece()
+		g.Update()
 		time.Sleep(time.Millisecond * 16)
 
 	}
